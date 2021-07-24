fa6ee76e77ad433bcfe3e4b626b08a8c509f1c42
MSV-School <MSV-School@MSV-SCHOOL>
2021-07-24 20:07:28 +0200

    Section 5 - Chapter 5 Display Named JSON...

--- js/main.js
+++ js/main.js
@@ -1,36 +1,32 @@
 // JavaScript Document
-//---------- Section 4: Loading External JSON Data ----------
 let xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'data.json', true);
 xhr.responseType = 'text';
+xhr.send();
 
-// tracking the data
-xhr.onreadystatechange = function() {
-    console.log(xhr.readyState); // 4 in the log means done (it works)
-    console.log(xhr.status); // 200 in the log means successful
-    console.log(xhr.statusText);
-}
-
-// will execute when readystate returns a 4
-xhr.onload = function() {
+xhr.onload = function(){
     if(xhr.status === 200) {
         let myStuff = JSON.parse(xhr.responseText);
         console.log(myStuff);
+
+        for(i = 0; i < myStuff.presidents.length; i++){
+            console.log('p: ', myStuff.presidents[i].first);
+            console.log('p: ', myStuff.presidents[i].last);
+            console.log('p: ', myStuff.presidents[i].served);
+
+            console.log('vp: ', myStuff.vicepresidents[i].first);
+            console.log('vp: ', myStuff.vicepresidents[i].last);
+        }
+        let myString = '';
+        for(i = 0; i < myStuff.presidents.length; i++){
+            let num = i + 1;
+            myString += '<br>President ' + num + ' was ';
+            myString += myStuff.presidents[i].first + ' ' + myStuff.presidents[i].last + '. ';
+            myString += 'He served from ' + myStuff.presidents[i].served + ' with ';
+
+            myString += myStuff.vicepresidents[i].first + ' ' + myStuff.vicepresidents[i].last;
+        }
+        document.getElementById('message').innerHTML = myString;
     }
 }
-
-xhr.send();
-
-//---------- Section 3: Exploring JSON ----------
-/*
-let theData1 = '{"Martin":{"age":"35", "degree":{"HVE":"Lexicon", "BA":"DU"}}, "Manjit":{"age":"61", "degree":"Basic"}}';
-
-let myObject = JSON.parse(theData1);
-console.log('obj --> ', myObject);
-
-let myString = JSON.stringify(myObject);
-console.log('string --> ', myString);
-
-document.getElementById('message').innerHTML = myObject.Martin.degree.HVE;
-*/
